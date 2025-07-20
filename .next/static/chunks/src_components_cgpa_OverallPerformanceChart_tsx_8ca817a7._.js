0e11bc0dfc41335641df5e43bffc80041bfd72e7
Sanjai-hue <sanjaikumar202007@gmail.com>
2025-07-20 08:48:48 +0530

    Add .next/static/chunks/src_components_cgpa_OverallPerformanceChart_tsx_8ca817a7._.js 1752981532497

---
+++ .next/static/chunks/src_components_cgpa_OverallPerformanceChart_tsx_8ca817a7._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_lodash_d59dde39._.js",
+  "static/chunks/node_modules_recharts_es6_4edf4f21._.js",
+  "static/chunks/node_modules_9d659e60._.js",
+  "static/chunks/src_components_cgpa_OverallPerformanceChart_tsx_ee5b2ae5._.js"
+],
+    source: "dynamic"
+});
