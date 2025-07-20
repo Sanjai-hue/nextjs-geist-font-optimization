a585316caf89666cfd60502f507212694ba1fe39
Sanjai-hue <sanjaikumar202007@gmail.com>
2025-07-20 08:49:00 +0530

    Add .next/static/chunks/src_components_cgpa_720d4456._.js 1752981544591

---
+++ .next/static/chunks/src_components_cgpa_720d4456._.js
@@ -0,0 +1,67 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
+
+"[project]/src/components/cgpa/CGPAPieChart.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_lodash_90f72504._.js",
+  "static/chunks/node_modules_recharts_es6_29f6c739._.js",
+  "static/chunks/node_modules_ba07ad27._.js",
+  "static/chunks/src_components_cgpa_CGPAPieChart_tsx_345d1391._.js",
+  "static/chunks/src_components_cgpa_CGPAPieChart_tsx_8f88d8fc._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/cgpa/CGPAPieChart.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+"[project]/src/components/cgpa/CGPABarChart.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_f20eecd1._.js",
+  "static/chunks/node_modules_ba07ad27._.js",
+  "static/chunks/src_components_cgpa_CGPABarChart_tsx_1aa4bc70._.js",
+  "static/chunks/src_components_cgpa_CGPABarChart_tsx_8f88d8fc._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/cgpa/CGPABarChart.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+"[project]/src/components/cgpa/SubjectFailureChart.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_lodash_20634561._.js",
+  "static/chunks/node_modules_recharts_es6_f20eecd1._.js",
+  "static/chunks/node_modules_ba07ad27._.js",
+  "static/chunks/src_components_cgpa_SubjectFailureChart_tsx_ab4b160b._.js",
+  "static/chunks/src_components_cgpa_SubjectFailureChart_tsx_8f88d8fc._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/cgpa/SubjectFailureChart.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+"[project]/src/components/cgpa/OverallPerformanceChart.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/node_modules_lodash_d59dde39._.js",
+  "static/chunks/node_modules_recharts_es6_4edf4f21._.js",
+  "static/chunks/node_modules_9d659e60._.js",
+  "static/chunks/src_components_cgpa_OverallPerformanceChart_tsx_ee5b2ae5._.js",
+  "static/chunks/src_components_cgpa_OverallPerformanceChart_tsx_8f88d8fc._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/cgpa/OverallPerformanceChart.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
